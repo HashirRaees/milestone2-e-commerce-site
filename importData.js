083274ecd602b2da4f46ca45bcf8b9ed27d9b15a
HashirRaees <hashirrais13@gmail.com>
2025-01-19 20:20:45 +0500

    commit

---
+++ importData.js
@@ -0,0 +1,86 @@
+import { createClient } from '@sanity/client';
+
+const client = createClient({
+  projectId: '2t2mr98m',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2025-01-13',
+  token: 'skvyynWiTnKoA52DGZHfLaBja5jwBe7lkUgzvTJrL55Iq0oi1PuM79xj2loifnCBybVInEl5l9bviBw8MemjATGlA1vzhcWLsVC640bxjPgC8St4kNC9WpzwpLpmYagf6B9rpLP3Y4EcxDz8E3JOS6arZoBjmWm6VSivcyaHRuKAcbOVQ3Wb',
+});
+
+async function uploadImageToSanity(imageUrl) {
+  try {
+    console.log(`Uploading image: ${imageUrl}`);
+
+    const response = await fetch(imageUrl);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image: ${imageUrl}`);
+    }
+
+    const buffer = await response.arrayBuffer();
+    const bufferImage = Buffer.from(buffer);
+
+    const asset = await client.assets.upload('image', bufferImage, {
+      filename: imageUrl.split('/').pop(),
+    });
+
+    console.log(`Image uploaded successfully: ${asset._id}`);
+    return asset._id;
+  } catch (error) {
+    console.error('Failed to upload image:', imageUrl, error);
+    return null;
+  }
+}
+
+async function uploadProduct(product) {
+  try {
+    const imageId = await uploadImageToSanity(product.imageUrl);
+
+    if (imageId) {
+      const document = {
+        _type: 'products',
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        image: {
+          _type: 'image',
+          asset: {
+            _ref: imageId,
+          },
+        },
+        category: product.category,
+        discountPercent: product.discountPercent,
+        isNew: product.isNew,
+        colors: product.colors,
+        sizes: product.sizes
+      };
+
+      const createdProduct = await client.create(document);
+      console.log(`Product ${product.name} uploaded successfully:`, createdProduct);
+    } else {
+      console.log(`Product ${product.name} skipped due to image upload failure.`);
+    }
+  } catch (error) {
+    console.error('Error uploading product:', error);
+  }
+}
+
+async function importProducts() {
+  try {
+    const response = await fetch('https://template1-neon-nu.vercel.app/api/products');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const products = await response.json();
+
+    for (const product of products) {
+      await uploadProduct(product);
+    }
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
+
+importProducts();
